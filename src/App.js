7ed9eb0be9389b3112afa62e7cf8f58b5e28433a
maq5ud <maksud@mudracircle.com>
2021-08-17 20:18:44 +0530

    feat: Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown'
 
 
 const items = [
@@ -19,11 +20,41 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The shade of Blue',
+        value: 'blue'
+    }
+]
+
 export default () => {
+
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
 
     return (
         <div>
-            <Search />
+            <button
+                onClick={() => setShowDropdown(!showDropdown)}
+            >
+                Toggle Dropdown
+            </button>
+            {showDropdown ?
+                <Dropdown 
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                    options={options}
+                />
+                : null
+            }
         </div>
     );
 
